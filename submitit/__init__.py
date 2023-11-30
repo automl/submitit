514090e5a1a9977b98ab1930a7d7c28e12e5e00b
SamuelGabriel <Samuel.g.mueller@web.de>
2023-11-30 20:37:59 +0100

    Update __init__.py

--- submitit/__init__.py
+++ submitit/__init__.py
@@ -20,7 +20,7 @@ from .slurm.slurm import SlurmExecutor as SlurmExecutor
 from .slurm.slurm import SlurmJob as SlurmJob
 from .slurm.slurm import SlurmInfoWatcher as SlurmInfoWatcher
 
-__version__ = "1.4.6"
+__version__ = "1.4.7"
 
 import subprocess
 import getpass
